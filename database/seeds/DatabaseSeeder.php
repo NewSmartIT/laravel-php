ab2f3292b0fce977277a9d8b8d4af6b663f0a8f5
NewSmartIT <manhtuanzai@gmail.com>
2016-08-08 10:55:12 +0700

    Add articles basic CRUD

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+    			$this->call(ArticleTableSeeder::class);
     }
+
+}
+class ArticleTableSeeder extends Seeder {
+
+	public function run()
+	{
+		DB::table('articles')->insert([
+			array('title'=>'Article Title','content'=>'Article Content'),
+			
+			]);
+	}
+	
+
 }
