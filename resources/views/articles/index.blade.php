b23301f1cf2137dfbdf91c250c89974e000c4cf3
NewSmartIT <manhtuanzai@gmail.com>
2016-08-09 11:48:24 +0700

    first commit

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,19 +8,21 @@
         <tr>
             <th>Edit</th>
             <th>Delete</th>
+            <th>Recommend</th>
             <th>Title</th>
             <th>Author</th>
         </tr>
         @foreach ($articles as $article)
             <tr>
-                <td>{!! link_to_route('articles.edit', 'Edit', $article->id,['class' => 'btn btn-default']) !!}</td>
-                <td>
-                    {!! Form::open(['method' => 'DELETE', 'route' => ['articles.destroy', $article->id]]) !!}
-                        <button type="submit" class="btn btn-warning">Delete</button>
-                    {!! Form::close() !!}
-                </td>
-                <td>{!! link_to_route('articles.show', $article->title, $article->id) !!}</td>
-                <td>{!! $article->author->name !!}</td>
+            <td>{!! link_to_route('articles.edit', 'Edit', $article->id, ['class' => 'btn btn-default']) !!}</td>
+            <td>
+                {!! Form::open(['method' => 'DELETE', 'route' => ['articles.destroy', $article->id]]) !!}
+                    <button type="submit" class="btn btn-warning">Delete</button>
+                {!! Form::close() !!}
+            </td>
+            <td>{!! link_to_route('articles.recommendations.create', 'Recommend', $article->id) !!}</td>
+            <td>{!! link_to_route('articles.show', $article->title, $article->id) !!}</td>
+            <td>{!! $article->author->name !!}</td>
             </tr>
         @endforeach
     </table>
